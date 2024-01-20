cd22da6122dbc0c06ea65bd550b23da425299a92
Prakash Kumar <singhprakash9572@gmail.com>
2024-01-21 01:08:56 +0530

    react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,12 @@
 import Chai from "./chai"
 function App() {
  
-
+const username="Chai aur code"
   return (
+    <>
 <Chai/>
+<h1> This is {username}</h1>
+</>
   )
 }
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+return(
+  <div>
+    <h1>
+      Custom App!
+    </h1>
+  </div>
+)
+}
+const username="Chai aur code"
+//Reactt Create Element
+
+const reactElement=React.createElement('a',{href:'https://arthum.co.in',target:'_blank'},'click to visit this Site ',)
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>
 )
