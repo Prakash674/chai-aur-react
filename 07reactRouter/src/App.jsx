6e2805609666a4a93e95a91e7f3aa9ea6ae9bfdc
Prakash Kumar <singhprakash9572@gmail.com>
2024-01-26 21:40:20 +0530

    react router

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Header from './components/Header/Header.jsx'
+import './App.css'
+import Footer from './components/Footer/Footer.jsx'
+import Home from './components/Home/Home.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* <h1 className='bg-green-600 p-4'>React Router</h1> */}
+
+    </>
+  )
+}
+
+export default App
